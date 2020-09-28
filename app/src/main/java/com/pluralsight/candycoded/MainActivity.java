a2d635a00f1bdac4944bf29550b69e011627568c
razan96 <razan.moh96@gmail.com>
2020-09-29 00:17:14 +0300

    module1-solution

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -5,11 +5,11 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -73,7 +73,14 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+    @Override 
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        Intent infoIntent = new Intent (this,InfoActivity.class );
+        startActivity(infoIntent);
+
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -83,14 +90,7 @@ public class MainActivity extends AppCompatActivity {
     // ***
     // TODO - Task 1 - Show Store Information Activity
     // ***  
-    @Override 
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        Intent infoIntent = new intent (this,InfoActivity.class );
-        startActivity(infoIntent);
-
-        return super.onOptionsItemSelected(item);
-}
+  
     private void addCandiesToDatabase(Candy[] candies) {
         SQLiteDatabase db = candyDbHelper.getWritableDatabase();
 
