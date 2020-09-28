412dc0e9c09e69ce67fdd46049688e9554765d5f
razan96 <razan.moh96@gmail.com>
2020-09-28 23:03:37 +0300

    1

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -82,9 +82,6 @@ public class MainActivity extends AppCompatActivity {
     // ***
     // TODO - Task 1 - Show Store Information Activity
     // ***
-
-   
-
 @Override 
 public boolean onOptionsItemSelected(MenuItem){
 
